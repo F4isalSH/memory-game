ddc6804d8feebe3b2c7152a9700f3bd27600d780
F4isalSH <f4isalcode@gmail.com>
2021-11-22 18:05:28 +0300

    Created grid for card images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css'
 import { useState } from 'react'
 function App() {
   const cardImages = [
-    {"src" : "../img/helmet-1.png"},
-    {"src" : "../img/helmet-1.png"},
-    {"src" : "../img/helmet-1.png"},
-    {"src" : "../img/helmet-1.png"},
-    {"src" : "../img/helmet-1.png"},
-    {"src" : "../img/helmet-1.png"}
+    {"src" : "/img/helmet-1.png"},
+    {"src" : "/img/potion-1.png"},
+    {"src" : "/img/ring-1.png"},
+    {"src" : "/img/scroll-1.png"},
+    {"src" : "/img/shield-1.png"},
+    {"src" : "/img/sword-1.png"}
   ]
 
   const [cards, setCards] = useState([])
@@ -16,14 +16,23 @@ function App() {
     const shuffledCards = [...cardImages, ...cardImages]
     .sort(()=> Math.random() - 0.5)
     .map((card)=> ({...card, id: Math.random()}))
-    setCards(shuffleCards)
+    setCards(shuffledCards)
     setTurns(0)
   }
+  console.log(cards)
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className='grid'>
+      {cards.map(card =>(
+        <div key = {card.id}>
+          <img src={card.src} alt="card element" />
+          <img src='/img/cover.png' alt="" />
+        </div>
+      ))}
     </div>
+      </div>
   );
 }
 
