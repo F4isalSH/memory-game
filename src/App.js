3aa1b7fb103b126f441b76b0174a0f12e24c3cb6
F4isalSH <f4isalcode@gmail.com>
2021-11-22 17:03:39 +0300

    Created function to shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import './App.css'
+import { useState } from 'react'
 function App() {
+  const cardImages = [
+    {"src" : "../img/helmet-1.png"},
+    {"src" : "../img/helmet-1.png"},
+    {"src" : "../img/helmet-1.png"},
+    {"src" : "../img/helmet-1.png"},
+    {"src" : "../img/helmet-1.png"},
+    {"src" : "../img/helmet-1.png"}
+  ]
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState()
+  const shuffleCards = () =>{
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(()=> Math.random() - 0.5)
+    .map((card)=> ({...card, id: Math.random()}))
+    setCards(shuffleCards)
+    setTurns(0)
+  }
   return (
     <div className="App">
       <h1>Memory Game</h1>
